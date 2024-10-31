223032b9ef149bea4a6dbc36ea9e2f6073886923
OneAutumLeafe <ayushsingh12072000@gmaile.com>
2024-11-01 02:46:43 +0530

    middleware_fix_commit

--- backend/routes/rideRoutes.js
+++ backend/routes/rideRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { createRide, getRides, updateRideStatus, updateLocation, getRideByTripId } = require('../controllers/rideController');
+const { createRide, getRides, updateRideStatus, updateLocation, getRideByTripId } = require('../controllers/rideController.js');
 const router = express.Router();
 
 // Authentication middleware can be added to protect these routes
-const { protect } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware.js');
 
 router.post('/create', protect, createRide);        // Create a new ride
 router.get('/', protect, getRides);                 // Get rides based on role
